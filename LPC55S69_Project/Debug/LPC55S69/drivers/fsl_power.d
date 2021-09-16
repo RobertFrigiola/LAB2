200a3463636829fac08f224eeaf8f46a39499878
RobertFrigiola <robertfrigiola@usf.edu>
2021-09-16 09:45:42 -0400

    Adding code outline of the first project

---
+++ LPC55S69_Project/Debug/LPC55S69/drivers/fsl_power.d
@@ -0,0 +1,32 @@
+LPC55S69/drivers/fsl_power.o LPC55S69/drivers/fsl_power.d: \
+ ../LPC55S69/drivers/fsl_power.c \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/fsl_device_registers.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/core_cm33.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_version.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_compiler.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_gcc.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/mpu_armv8.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/system_LPC55S69_cm33_core0.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0_features.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common_arm.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_clock.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common.h \
+ C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_reset.h \
+ ../LPC55S69/drivers/fsl_power.h
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/fsl_device_registers.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/core_cm33.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_version.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_compiler.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/cmsis_gcc.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\CMSIS/mpu_armv8.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/system_LPC55S69_cm33_core0.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\device/LPC55S69_cm33_core0_features.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common_arm.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_clock.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_common.h:
+C\:\Users\RJ\Documents\MCUXpressoIDE_11.4.0_6237\workspace\LPC55S69_Project\drivers/fsl_reset.h:
+../LPC55S69/drivers/fsl_power.h:
